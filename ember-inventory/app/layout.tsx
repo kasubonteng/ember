ce610f9843ac2d207e49b3b383f8e719da601719
Kwadwo Asubonteng <kasubonteng2002@gmail.com>
2024-09-06 20:59:49 +0000

    feat: initial project setup

---
+++ ember-inventory/app/layout.tsx
@@ -0,0 +1,68 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { TooltipProvider } from "@/components/ui/tooltip";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+const gilroy = localFont({
+  src: [
+    {
+      path: "./fonts/Gilroy-Light.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Gilroy-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Gilroy-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Gilroy-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Gilroy-Heavy.ttf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  display: "swap",
+  variable: "--font-gilroy",
+});
+
+export const metadata: Metadata = {
+  title: "Ember Inventory",
+  description: "Inventory management for Ember furniture",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${gilroy.variable} antialiased`}
+      >
+        <TooltipProvider>{children}</TooltipProvider>
+      </body>
+    </html>
+  );
+}
---
+++ ember-inventory/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+	return (
+		<div className="min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+			<main className="flex flex-col gap-8 items-center">
+				<h1>hello world</h1>
+			</main>
+		</div>
+	);
+}
